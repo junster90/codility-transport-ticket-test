3d4e528e7a3175b3402521a4f54199ff869f069c
Snowflakekiller <karuna.skj@gmail.com>
2016-08-06 20:38:51 +0800

    style fix

--- solution.rb
+++ solution.rb
@@ -8,30 +8,27 @@ def solution(a)
 
   return 25 if input.length >= 23
 
-  until input.length == 0 do
+  while input.zero?
     today = input.first
 
     end_of_week = today + 6
-    week_utilization = input.select {|x| x <= end_of_week }
+    week_utilization = input.select { |x| x <= end_of_week }
 
     better_op_exists = false
 
     week_utilization.each do |day|
       last_day = day + 6
-      potential = input.select {|x| x >= day && x <= last_day}
-      if potential.length > week_utilization.length
-        better_op_exists =  true
-      end
+      potential = input.select { |x| x >= day && x <= last_day }
+      better_op_exists =  true if potential.length > week_utilization.length
     end
 
     if week_utilization.length >= 4 && !better_op_exists
       cost += weekly_ticket
-      input.reject! { |x| x <= end_of_week}
+      input.reject! { |x| x <= end_of_week }
     else
       cost += daily_ticket
       input.delete(today)
     end
   end
-
   cost
 end
